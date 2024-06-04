b9aeb3d039850d2677361c7dbb6b798f386d5b9e
thulasicse <142043432+thulasicse@users.noreply.github.com>
2024-06-04 22:26:38 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,29 @@
+fahrenheit = document.querySelector("#fahrenheit-ip");
+celsius = document.querySelector("#celsius-ip");
+tempConvert = document.querySelector("#temp-conv-btn");
+
+
+
+var tempSwitch = true
+var lengthSwitch = 1
+
+function changeFocus1(val) {
+	tempSwitch = val;
+}
+
+function changeFocus2(val) {
+	lengthSwitch = val;
+}
+
+tempConvert.addEventListener
+(
+	"click",
+	() =>
+	{
+		if(tempSwitch == 1)
+			fahrenheit.value = (parseInt(celsius.value) * 1.8) + 32;
+		else
+			celsius.value = (parseInt(fahrenheit.value) - 32) / 1.8;
+	}
+);
+
